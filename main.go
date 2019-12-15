dc74b1ba6e7ee2d358c96c00d56864ca62c756c2
CrazyMax <crazy-max@users.noreply.github.com>
2019-12-15 04:18:30 +0100

    Add cleanup config

--- main.go
+++ main.go
@@ -4,20 +4,31 @@ package main
 
 import (
 	"os"
+	"path"
 
 	. "github.com/portapps/portapps"
 	"github.com/portapps/portapps/pkg/utl"
 )
 
+type config struct {
+	Cleanup bool `yaml:"cleanup" mapstructure:"cleanup"`
+}
+
 var (
 	app *App
+	cfg *config
 )
 
 func init() {
 	var err error
 
+	// Default config
+	cfg = &config{
+		Cleanup: false,
+	}
+
 	// Init app
-	if app, err = New("beaker-browser-portable", "Beaker Browser"); err != nil {
+	if app, err = NewWithCfg("beaker-browser-portable", "Beaker Browser", cfg); err != nil {
 		Log.Fatal().Err(err).Msg("Cannot initialize application. See log file for more info.")
 	}
 }
@@ -25,6 +36,15 @@ func init() {
 func main() {
 	utl.CreateFolder(app.DataPath)
 	app.Process = utl.PathJoin(app.AppPath, "Beaker Browser.exe")
+
+	// Cleanup on exit
+	if cfg.Cleanup {
+		defer func() {
+			utl.Cleanup([]string{
+				path.Join(os.Getenv("APPDATA"), "Beaker Browser"),
+			})
+		}()
+	}
 
 	app.Launch(os.Args[1:])
 }
